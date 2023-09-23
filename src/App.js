686ca029ccb05a84b8567ce660704b71983c9000
AST E <Eukarya-est@gamil.com>
2023-09-23 17:37:02 +0900

    Create React  Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <h2>Management system</h2>
+      <img src={logo} alt="logo"/>
+
+
+    </div>
+  );
+}
+
+export default App;
